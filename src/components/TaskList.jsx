d8456bc84c288f59dc076f900d6a155effe618ae
mopia <shaddiemopiah35@gmail.com>
2022-06-17 10:26:22 +0300

    Added InboxScreen component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Task from "./Task";
-import { useDispatch, useSelector } from "react-redux";
-import { updateTaskState } from "../lib/store";
+import React from 'react';
+import Task from './Task';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTaskState } from '../lib/store';
 
 export default function TaskList() {
   // We're retrieving our state from the store
   const tasks = useSelector((state) => {
     const tasksInOrder = [
-      ...state.taskbox.tasks.filter((t) => t.state === "TASK_PINNED"),
-      ...state.taskbox.tasks.filter((t) => t.state !== "TASK_PINNED"),
+      ...state.taskbox.tasks.filter((t) => t.state === 'TASK_PINNED'),
+      ...state.taskbox.tasks.filter((t) => t.state !== 'TASK_PINNED'),
     ];
     const filteredTasks = tasksInOrder.filter(
-      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
     );
     return filteredTasks;
   });
@@ -22,23 +22,23 @@ export default function TaskList() {
 
   const pinTask = (value) => {
     // We're dispatching the Pinned event back to our store
-    dispatch(updateTaskState({ id: value, newTaskState: "TASK_PINNED" }));
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_PINNED' }));
   };
   const archiveTask = (value) => {
     // We're dispatching the Archive event back to our store
-    dispatch(updateTaskState({ id: value, newTaskState: "TASK_ARCHIVED" }));
+    dispatch(updateTaskState({ id: value, newTaskState: 'TASK_ARCHIVED' }));
   };
   const LoadingRow = (
-    <div className="loading-item">
-      <span className="glow-checkbox" />
-      <span className="glow-text">
+    <div className='loading-item'>
+      <span className='glow-checkbox' />
+      <span className='glow-text'>
         <span>Loading</span> <span>cool</span> <span>state</span>
       </span>
     </div>
   );
-  if (status === "loading") {
+  if (status === 'loading') {
     return (
-      <div className="list-items" data-testid="loading" key={"loading"}>
+      <div className='list-items' data-testid='loading' key={'loading'}>
         {LoadingRow}
         {LoadingRow}
         {LoadingRow}
@@ -50,18 +50,18 @@ export default function TaskList() {
   }
   if (tasks.length === 0) {
     return (
-      <div className="list-items" key={"empty"} data-testid="empty">
-        <div className="wrapper-message">
-          <span className="icon-check" />
-          <div className="title-message">You have no tasks</div>
-          <div className="subtitle-message">Sit back and relax</div>
+      <div className='list-items' key={'empty'} data-testid='empty'>
+        <div className='wrapper-message'>
+          <span className='icon-check' />
+          <div className='title-message'>You have no tasks</div>
+          <div className='subtitle-message'>Sit back and relax</div>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="list-items" data-testid="success" key={"success"}>
+    <div className='list-items' data-testid='success' key={'success'}>
       {tasks.map((task) => (
         <Task
           key={task.id}
